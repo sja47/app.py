f9b97049147379db2c8058a5bbb8ca9292097fb5
sja47 <serge.arahal@gmail.com>
2024-09-22 16:02:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,5 +12,15 @@ st.title("Exploring Tourism and Accommodation Trends in Selected Lebanese Towns"
 Tourism_Index_COLUMN = 'Tourism Index'
 Number_of_Hotels_COLUMN = 'Total_number_of_hotels'
 data_csv='551015b5649368dd2612f795c2a9c2d8_20240902_115953.csv'
- 
+def load_data(nrows):
+  data = pd.read_csv(data_csv, nrows=nrows)
+  #lowercase = lambda x: str(x).lower()
+  #data.rename(lowercase, axis='columns', inplace=True)
+  #data[Tourism_Index_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+  return data
+
+data_load_state = st.text('Loading data...')
+data = load_data(50)
+data_load_state.text("Done! (using st.cache_data)")
+
 
