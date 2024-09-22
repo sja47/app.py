781e5bf5f23c4b14698050f2b773e8d9644e8f5b
sja47 <serge.arahal@gmail.com>
2024-09-22 16:16:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -27,5 +27,13 @@ st.markdown("In this interactive dashboard, we explore two important aspects of 
 st.markdown("Tourism Index:The Tourism Index measures a town's attractiveness to tourists, considering factors like attractions, culture, and infrastructure. A higher score indicates a greater ability to attract visitors, boosting economic activity and hospitality growth.")
 st.markdown("Number of Hotels:The number of hotels in a town reflects its accommodation capacity. Towns with more hotels are better positioned to welcome tourists, highlighting their dependence on tourism for economic purposes.")
 st.subheader('Number of hotels By Town')
-
+Hotels_to_filter = st.slider('Number of Hotels', 0, 4, 0)
+filtered_data = data[Tourism_Index_COLUMN ][data[Number_of_Hotels_COLUMN] == Hotels_to_filter]
+chart_data = pd.DataFrame(
+    {
+        "Town": data['Town'],
+        "Hotels": data[Number_of_Hotels_COLUMN][data[Number_of_Hotels_COLUMN]==Hotels_to_filter]
+  
+    }
+)
 
