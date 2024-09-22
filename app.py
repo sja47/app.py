8c8a1b7284f6ba345ca68a0c8cec6a00b98d9c66
sja47 <serge.arahal@gmail.com>
2024-09-22 17:06:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,15 +19,11 @@ def load_data(nrows):
   #data.rename(lowercase, axis='columns', inplace=True)
   #data[Tourism_Index_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
   return data
-  data_load_state = st.text('Loading data...')
+  
+data_load_state = st.text('Loading data...')
 data = load_data(50)
 data_load_state.text("Done! (using st.cache_data)")
-def load_data(nrows):
-  data = pd.read_csv(data_csv, nrows=nrows)
-  #lowercase = lambda x: str(x).lower()
-  #data.rename(lowercase, axis='columns', inplace=True)
-  #data[Tourism_Index_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-  return data
+
 if st.checkbox('Show raw data'):
   st.subheader('Raw data')
   st.write(data)
