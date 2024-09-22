7b12e195e1556483ecd182654de8d23f58032d8d
sja47 <serge.arahal@gmail.com>
2024-09-22 16:03:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,5 @@ def load_data(nrows):
   #data[Tourism_Index_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
   return data
 
-data_load_state = st.text('Loading data...')
-data = load_data(50)
-data_load_state.text("Done! (using st.cache_data)")
 
 
