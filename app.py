2e3218a67a9f94279780307352c805472495692e
sja47 <serge.arahal@gmail.com>
2024-09-22 16:58:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import pandas as pd
 import numpy as np
 st.title("Exploring Tourism and Accommodation Trends in Selected Lebanese Towns")
 Tourism_Index_COLUMN = 'Tourism Index'
-Number_of_Hotels_COLUMN = 'Total_number_of_hotels'
+Number_of_Hotels_COLUMN = 'Total number of hotels'
 data_csv='551015b5649368dd2612f795c2a9c2d8_20240902_115953.csv'
 def load_data(nrows):
   data = pd.read_csv(data_csv, nrows=nrows)
@@ -27,7 +27,5 @@ st.markdown("Tourism Index:The Tourism Index measures a town's attractiveness to
 st.markdown("Number of Hotels:The number of hotels in a town reflects its accommodation capacity. Towns with more hotels are better positioned to welcome tourists, highlighting their dependence on tourism for economic purposes.")
 st.subheader('Number of hotels By Town')
 Hotels_to_filter = st.slider('Number of Hotels', 0, 4, 0)
-chart_data = pd.DataFrame({
-    "Town": data['Town'][data[Number_of_Hotels_COLUMN] == Hotels_to_filter],
-    "Hotels": data[Number_of_Hotels_COLUMN][data[Number_of_Hotels_COLUMN] == Hotels_to_filter]
-}).reset_index(drop=True)
+filtered_data = data[Tourism_Index_COLUMN ][data[Number_of_Hotels_COLUMN] == Hotels_to_filter]
+
