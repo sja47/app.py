e101b03c3034a1dc83fb610ca64b376ca3d3449f
sja47 <serge.arahal@gmail.com>
2024-09-22 15:53:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ import numpy as np
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
 st.title("Exploring Tourism and Accommodation Trends in Selected Lebanese Towns")
 Tourism_Index_COLUMN = 'Tourism Index'
 Number_of_Hotels_COLUMN = 'Total_number_of_hotels'
