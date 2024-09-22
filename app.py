db51d81374c42a6082d5812beaec03d47aece9b6
sja47 <serge.arahal@gmail.com>
2024-09-22 16:00:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ print(df.head(50))
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+st.title("Exploring Tourism and Accommodation Trends in Selected Lebanese Towns")
+Tourism_Index_COLUMN = 'Tourism Index'
+Number_of_Hotels_COLUMN = 'Total_number_of_hotels'
+data_csv='551015b5649368dd2612f795c2a9c2d8_20240902_115953.csv'
+@st.cache_data  
 
