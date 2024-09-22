6516e2c1533bc9811850eacdd37c2749a49e58b3
sja47 <serge.arahal@gmail.com>
2024-09-22 17:17:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pandas as pd
-data_url = 'https://github.com/sja47/app.py/blob/main/551015b5649368dd2612f795c2a9c2d8_20240902_115953.csv'
+data_url = '551015b5649368dd2612f795c2a9c2d8_20240902_115953.csv'
 
 # Load the CSV file directly from the URL
 df = pd.read_csv(data_url)  # Load the CSV file
