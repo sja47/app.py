b2c627e2a2633e429e61f35bfcdbd29365ca4a71
sja47 <serge.arahal@gmail.com>
2024-09-22 13:06:14 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ pip install plotly-express
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import plotly.express as px
 
 st.title("Exploring Tourism and Accommodation Trends in Selected Lebanese Towns")
