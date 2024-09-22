2c67b89144854125c83d724c24299b3173ed4a99
sja47 <serge.arahal@gmail.com>
2024-09-22 15:45:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pandas as pd
 uploaded = files.upload()
-for filename in uploaded.keys():
+for filename in uploaded.keys(https://linked.aub.edu.lb/pkgcube/data/551015b5649368dd2612f795c2a9c2d8_20240902_115953.csv):
     df = pd.read_csv(filename)  # Load the CSV file
     print(f"Loaded {filename} into DataFrame.")
