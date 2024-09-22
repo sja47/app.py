71204f3db6cd8e7f405bc9838ef1031b4e66b09c
sja47 <serge.arahal@gmail.com>
2024-09-22 15:43:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import pandas as pd
 from google.colab import files
 uploaded = files.upload()
-for filename in uploaded.keys():
-    df = pd.read_csv(filename)  # Load the CSV file
-    print(f"Loaded {filename} into DataFrame.")
+
